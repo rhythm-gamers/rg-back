d743702c568dc6ab3f3e31df347b7a4f6bca1414
PromotezCitizen <gkstkdgus821@gmail.com>
2024-02-22 22:51:16 +0900

    모듈에서 TypeOrm에 접근할 수 있게 수정

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { CommentService } from "./comment.service";
 import { CommentController } from "./comment.controller";
+import { Comment } from "./entity/comment.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   controllers: [CommentController],
   providers: [CommentService],
+  imports: [TypeOrmModule.forFeature([Comment])],
+  exports: [CommentService],
 })
 export class CommentModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,18 @@
 import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entity/user.entity";
+import { PatternProgress } from "./entity/pattern-progress.entity";
+import { PlateSetting } from "./entity/plate-setting.entity";
+import { UserTitle } from "./entity/user-title.entity";
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
+  imports: [
+    TypeOrmModule.forFeature([User, PatternProgress, PlateSetting, UserTitle]),
+  ],
   exports: [UserService],
 })
 export class UserModule {}
