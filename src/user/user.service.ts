1331f1f08f8d2bd254a08b56ba177c99353450aa
PromotezCitizen <gkstkdgus821@gmail.com>
2024-02-25 19:15:43 +0900

    유저 id로 user 객체 받아오는 서비스 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./entity/user.entity";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async fetchUserWithUserID(user_id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({
+      user_id: user_id,
+    });
+    return user;
+  }
+}
