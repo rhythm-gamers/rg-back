d66bcf1249b99b0f44378b24ba6e51dbd4784707
PromotezCitizen <gkstkdgus821@gmail.com>
2024-03-12 23:40:14 +0900

    Refactory: S3 DTO 수정
    modify: dto 파일 및 폴더 이름 변경
    delete: s3 controller 삭제 ← 사용하지 않는데 남겨둘 필요 없음

--- src/s3/aws-s3.module.ts
+++ src/s3/aws-s3.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AwsS3Service } from './aws-s3.service';
-import { AwsS3Controller } from './aws-s3.controller';
 
 @Module({
-  controllers: [AwsS3Controller],
   providers: [AwsS3Service],
   exports: [AwsS3Service],
 })
--- src/s3/dto/aws-s3-move-file.dto.ts
+++ src/s3/dto/aws-s3-move-file.dto.ts
@@ -1,3 +1,3 @@
-import { AwsS3CopyFileDto } from './aws-s3-copy-files.dto';
+import { AwsS3CopyFileDto } from './aws-s3-copy-file.dto';
 
 export interface AwsS3MoveFileDto extends AwsS3CopyFileDto {}
