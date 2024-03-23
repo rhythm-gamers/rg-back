faf75b054d607bc6809e7e9593829d6ef1c37531
PromotezCitizen <gkstkdgus821@gmail.com>
2024-03-23 21:01:58 +0900

    Feat: wiki CRUD 기능 구현 1. wiki.controller.ts - Feat: swagger용 ApiPram, ApiResponse 추가 - Fix: admin만 C_UD 가능하게 @Roles 데코레이터 추가 2. wiki.entity.ts - Fix: uid를 wikiId로 수정 3. wiki.service.ts - Refactor: get을 fetch로 이름 변경 - Refactor: 함수 이름 수정 - Fix: _RUD 시 title을 기준으로 받아오도록 수정 - Feat: 각 함수에서 해당 데이터가 없는 경우 Exception을 반환하는 코드 추가 4. swagger-properties.obj.ts - Create: swagger용 Response Property 지정 5. **-data.*.ts - Refactor: '-data'를 파일 및 클래스 이름에서 제거 6. return-wiki.dto.ts, wiki-metadata.obj.ts - Delete: class validator가 불필요함에 따라 class validator 삭제 7. create-wiki.dto.ts, update-wiki.dto.ts - Append: @ApiProperty에 example 추가

--- src/wiki/dto/update-wiki-data.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/swagger";
-import { CreateWikiDataDto } from "./create-wiki-data.dto";
-
-export class UpdateWikiDataDto extends PartialType(CreateWikiDataDto) {}
--- src/wiki/wiki.controller.ts
+++ src/wiki/wiki.controller.ts
@@ -3,19 +3,34 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
+  Patch,
   Post,
-  Put,
+  Res,
 } from "@nestjs/common";
 import { WikiService } from "./wiki.service";
 import { WikiMetadataOrigin } from "./obj/wiki-metadata-origin.obj";
 import { ReturnWikiMetadataDto } from "./dto/return-wiki-metadata.dto";
-import { ReturnWikiDataDto } from "./dto/return-wiki-data.dto";
-import { CreateWikiDataDto } from "./dto/create-wiki-data.dto";
-import { UpdateWikiDataDto } from "./dto/update-wiki-data.dto";
-import { DeleteWikiDataDto } from "./dto/delete-wiki-data.dto";
-import { ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger";
-import { SkipAuth } from "src/token/token.metadata";
+import { ReturnWikiDto } from "./dto/return-wiki.dto";
+import { CreateWikiDto } from "./dto/create-wiki.dto";
+import { UpdateWikiDto } from "./dto/update-wiki.dto";
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from "@nestjs/swagger";
+import { Roles, SkipAuth } from "src/token/token.metadata";
+import { Role } from "src/auth/object/token-payload.obj";
+import { Response } from "express";
+import {
+  metadataOkProperty,
+  wikiCreateProperties,
+  wikiProperties,
+} from "./obj/swagger-properties.obj";
 
 @Controller("wiki")
 @ApiTags("wiki")
@@ -23,71 +38,167 @@ export class WikiController {
   constructor(private readonly wikiService: WikiService) {}
 
   @SkipAuth()
+  @ApiOperation({ summary: "위키 메타데이터 가져오기" })
   @Get("metadata")
-  @ApiOperation({})
-  async getWikiMetadata(): Promise<ReturnWikiMetadataDto> {
+  @ApiOkResponse({
+    description: "메타데이터(제목, 필독 여부) 반환",
+    schema: {
+      type: "object",
+      properties: metadataOkProperty(),
+    },
+  })
+  async fetchWikiMetadata(@Res() res: Response) {
     const metadatas: WikiMetadataOrigin[] =
-      await this.wikiService.getWikiMetadata();
-
-    const result: ReturnWikiMetadataDto = {};
+      await this.wikiService.fetchMetadata();
+    const result: ReturnWikiMetadataDto = {
+      mustread: [],
+    };
     metadatas.forEach((metadata) => {
-      if (!result[metadata.letter]) {
+      if (metadata.mustRead === true) {
+        result.mustread.push(this.generateMetadata(metadata));
+      }
+      if (result[metadata.letter] == null) {
         result[metadata.letter] = [];
       }
-      result[metadata.letter].push({
-        uid: metadata.uid,
-        title: metadata.title,
-        mustRead: metadata.mustRead,
-      });
+      result[metadata.letter].push(this.generateMetadata(metadata));
     });
-    return result;
+    res.status(HttpStatus.OK).send(result);
+  }
+
+  private generateMetadata(metadata: WikiMetadataOrigin) {
+    return {
+      wikiId: metadata.wikiId,
+      title: metadata.title,
+      mustRead: metadata.mustRead,
+    };
   }
 
   @SkipAuth()
-  @Get("spec/:id")
+  @ApiOperation({ summary: "위키 내용 가져오기" })
+  @Get("spec/:title")
   @ApiParam({
-    name: "위키 id",
+    name: "title",
     required: true,
+    example: "test",
+    description: "가져오려는 위키 제목",
   })
-  @ApiOperation({})
-  async getWikiData(@Param("id") id: number): Promise<ReturnWikiDataDto> {
-    const result: ReturnWikiDataDto = await this.wikiService.getWikiData(id);
-    return result;
-  }
-
-  // TODO Auth 필요
-  @Post()
-  @ApiOperation({})
-  async createtWikiData(@Body() wiki: CreateWikiDataDto) {
-    const result = await this.wikiService.createWikiData(wiki);
-    return result;
-  }
-
-  // TODO Auth 필요
-  @Put(":originTitle")
-  @ApiOperation({})
-  async updateWikiData(
-    @Body() wiki: UpdateWikiDataDto,
-    @Param("originTitle") originTitle: string,
-  ) {
-    const result = await this.wikiService.updateWikiDataByTitle(
-      wiki,
-      originTitle,
-    );
-    return result;
-  }
-
-  // TODO Auth 필요
-  @Delete()
-  @ApiOperation({ description: "제목 또는 인덱스로 제거할 수 있도록 함" })
-  async deleteWikiData(@Body() wikiDelete: DeleteWikiDataDto) {
-    let result;
-    if (wikiDelete.title) {
-      result = await this.wikiService.deleteWikiDataByTitle(wikiDelete.title);
-    } else if (wikiDelete.wikiId) {
-      result = await this.wikiService.deleteWikiDataById(+wikiDelete.wikiId);
+  @ApiOkResponse({
+    description: "위키 데이터 반환",
+    schema: {
+      type: "object",
+      properties: wikiProperties(),
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "해당 데이터 없음",
+    schema: {
+      type: "string",
+      example: "해당 위키 데이터는 없습니다.",
+    },
+  })
+  async fetchWikiData(@Param("title") title: string, @Res() res: Response) {
+    const result: ReturnWikiDto = await this.wikiService.fetch(title);
+    if (result) {
+      res.status(HttpStatus.OK).send(result);
+    } else {
+      res.status(HttpStatus.BAD_REQUEST).send("해당 위키 데이터는 없습니다.");
     }
+  }
 
-    return result;
+  @Roles(Role.Admin)
+  @Post()
+  @ApiOperation({ summary: "위키 생성" })
+  @ApiOkResponse({
+    description: "생성 성공",
+    schema: {
+      type: "object",
+      properties: wikiCreateProperties(),
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "이미 있는 데이터",
+    schema: {
+      type: "string",
+      example: "이미 있는 위키입니다.",
+    },
+  })
+  async createtWikiData(@Body() wiki: CreateWikiDto, @Res() res: Response) {
+    const result = await this.wikiService.create(wiki);
+    res.status(HttpStatus.OK).send(result);
+  }
+
+  @Roles(Role.Admin)
+  @Patch(":originTitle")
+  @ApiOperation({ summary: "위키 내용 업데이트" })
+  @ApiParam({
+    name: "originTitle",
+    description: "업데이트 하는 위키의 원래 이름",
+    required: true,
+    example: "test",
+  })
+  @ApiOkResponse({
+    description: "수정 성공",
+    schema: {
+      type: "object",
+      properties: {
+        generatedMaps: { type: "array", example: [] },
+        raw: { type: "array", example: [] },
+        affected: { type: "number", example: "1" },
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "수정 실패 - 바꾸려는 위키가 없음",
+    schema: {
+      type: "string",
+      example: "해당 위키는 없습니다.",
+    },
+  })
+  @ApiConflictResponse({
+    description: "수정 실패 - 바꾸려는 위키의 제목이 이미 존재함",
+    schema: {
+      type: "string",
+      example: "바꾸려는 위키의 제목이 이미 존재합니다.",
+    },
+  })
+  async updateWikiData(
+    @Body() wiki: UpdateWikiDto,
+    @Param("originTitle") originTitle: string,
+    @Res() res: Response,
+  ) {
+    const result = await this.wikiService.updateByTitle(wiki, originTitle);
+    res.status(200).send(result);
+  }
+
+  @Roles(Role.Admin)
+  @Delete(":title")
+  @ApiOperation({ summary: "제목으로 제거할 수 있도록 함" })
+  @ApiParam({
+    name: "title",
+    description: "삭제하려는 위키의 제목",
+    required: true,
+    example: "test",
+  })
+  @ApiOkResponse({
+    description: "삭제 성공",
+    schema: {
+      type: "string",
+      example: "삭제 성공",
+    },
+  })
+  @ApiBadRequestResponse({
+    description: "삭제 실패 or 데이터베이스에 없는 위키",
+    schema: {
+      type: "string",
+      example: "해당 위키는 없습니다.",
+    },
+  })
+  async deleteWikiData(@Param("title") title: string, @Res() res: Response) {
+    const result = await this.wikiService.deleteByTitle(title);
+    if (result.affected === 0) {
+      res.status(HttpStatus.NOT_FOUND).send("데이터베이스에 없는 위키");
+    } else {
+      res.status(HttpStatus.OK).send("제거 성공");
+    }
   }
 }
