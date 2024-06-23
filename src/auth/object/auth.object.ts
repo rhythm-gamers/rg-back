26d610ba000c0e967c4f82429ec8c765a2a123a7
PromotezCitizen <gkstkdgus821@gmail.com>
2024-06-23 13:16:51 +0900

    Deprecated: 리듬게임 목록을 담는 배열을 칭호의 Object로 이동

--- src/auth/object/auth.object.ts
+++ src/auth/object/auth.object.ts
@@ -10,12 +10,3 @@ export type SteamUserObject = {
     large: string;
   };
 };
-
-export const rhythmGameList = [
-  774171, // 뮤즈대시
-  774171, // 리듬닥터
-  960170, // 디맥
-  977950, // 얼불춤
-  1477590, // 투온
-  1802720, // 식스타
-];
