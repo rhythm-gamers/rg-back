b93b810e06a2e878d8a8fab002aae3a5a77d7f09
PromotezCitizen <gkstkdgus821@gmail.com>
2024-09-14 22:03:00 +0900

    Update: Swagger Docs에서 V1, V2를 확인할 수 있도록 수정

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -3,21 +3,42 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import expressBasicAuth from "express-basic-auth";
 
 export const setupSwagger = (app: INestApplication) => {
+  const version = "0.3";
   const config = new DocumentBuilder()
     .setTitle("리듬게이머즈")
     .setDescription("개발용 API 리스트")
-    .setVersion("0.2")
+    .setVersion(version)
+    .addTag("auth", "register/login/logout operation")
+    .addTag("User Setting", "user operation")
+    .addTag("칭호", "chingho opretaion")
+    .addTag("steam", "steam operation")
+    .addTag("progress", "progress operation")
     .addTag("level test", "level test operation")
     .addTag("practice", "practice operation")
     .addTag("wiki", "wiki operation")
+    .addTag("board", "board operation")
     .addTag("post", "post operation")
     .addTag("comment", "comment operation")
-    .addTag("board", "board operation")
-    .addTag("progress", "progress operation")
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  const documentV1 = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/v1", app, documentV1);
+
+  const v2 = new DocumentBuilder()
+    .setTitle("리듬게이머즈 v2")
+    .setDescription("개발용 v2 api 리스트")
+    .setVersion(version)
+    .addTag("auth", "register/login/logout operation")
+    .addTag("ruser", "user opetation")
+    .addTag("rboard", "board operation")
+    .addTag("rpost", "post operation")
+    .addTag("rcomment", "comment operation")
+    .addTag("rchingho", "chingho operation")
+    .addBearerAuth()
+    .build();
+  const documentV2 = SwaggerModule.createDocument(app, v2);
+  SwaggerModule.setup("/api/v2", app, documentV2);
+
   app.use(
     ["api"],
     expressBasicAuth({
