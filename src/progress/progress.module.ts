77b0b9b0f3c28c6d619de98c8b6439668e8f0039
PromotezCitizen <gkstkdgus821@gmail.com>
2024-09-22 15:50:42 +0900

    Add: v2 service, controller를 module에 추가, 추후 v2 module로 변경

--- src/progress/progress.module.ts
+++ src/progress/progress.module.ts
@@ -7,22 +7,37 @@ import { PracticeProgressService } from "./service/practice-progress.service";
 import { ProgressService } from "./service/progress.service";
 import { UserModule } from "src/user/user.module";
 import { PatternModule } from "src/pattern/pattern.module";
-import { ProgressController } from "./progress.controller";
+import { ProgressController } from "./controller/progress.controller";
 import { FirebaseModule } from "src/firebase/firebase.module";
+import { PlateModule } from "src/plate/plate.module";
+import { RLevelTestProgress } from "./entity/rlevel-test-progress.entity";
+import { RPracticeProgress } from "./entity/rpractice-progress.entity";
+import { RProgressController } from "./controller/rprogress.controller";
+import { RLevelTestProgressService } from "./service/rlevel-test-progress.service";
+import { RProgressService } from "./service/rprogress.service";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([LevelTestProgress, PracticeProgress]),
+    TypeOrmModule.forFeature([
+      LevelTestProgress,
+      PracticeProgress,
+      RLevelTestProgress,
+      RPracticeProgress,
+    ]),
     forwardRef(() => UserModule),
     forwardRef(() => PatternModule),
+    PlateModule,
     FirebaseModule,
   ],
-  controllers: [ProgressController],
+  controllers: [ProgressController, RProgressController],
   providers: [
     LevelTestProgressService,
     PracticeProgressService,
     ProgressService,
+    RLevelTestProgressService,
+    RPracticeProgress,
+    RProgressService,
   ],
-  exports: [ProgressService],
+  exports: [ProgressService, RProgressService],
 })
 export class ProgressModule {}
