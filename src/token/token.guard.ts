b5d5fe63e426871bb8dfc7a9e8bff348bdeeb375
YongjaeHyun <dltjrrbs2020@gmail.com>
2024-10-21 00:53:19 +0900

    Feat: header 방식 가드 추가

--- src/token/token.guard.ts
+++ src/token/token.guard.ts
@@ -12,8 +12,12 @@ import { TokenService } from "./token.service";
 import { cookieOptions } from "./cookie.options";
 
 type Cookies = {
-  access_token: string;
-  refresh_token: string;
+  access_token?: string;
+  refresh_token?: string;
+};
+type Headers = {
+  access_token?: string;
+  refresh_token?: string;
 };
 
 @Injectable()
@@ -31,31 +35,45 @@ export class TokenGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const response = context.switchToHttp().getResponse() as Response;
     const cookies: Cookies = request.cookies as Cookies;
+    const headers: Headers = request.headers as Headers;
 
-    if (!cookies.access_token || !cookies.refresh_token) {
+    const notExistTokenAtCookies = !cookies.access_token || !cookies.refresh_token
+    const notExistTokenAtHeaders = !headers.access_token || !headers.refresh_token
+    
+    if (notExistTokenAtCookies && notExistTokenAtHeaders) {
       if (!isPublic) throw new UnauthorizedException("Token not found");
     }
 
     try {
-      cookies.access_token = cookies.access_token.replace("Bearer ", "");
-      cookies.refresh_token = cookies.refresh_token.replace("Bearer ", "");
+      if(cookies.access_token){
+        cookies.access_token = cookies.access_token.replace("Bearer ", "");
+        cookies.refresh_token = cookies.refresh_token.replace("Bearer ", "");
+      }else {
+        headers.access_token = headers.access_token.replace("Bearer ", "");
+        headers.refresh_token = headers.refresh_token.replace("Bearer ", "");
+      }
     } catch (err) {}
 
     try {
+      const currentAccessToken = cookies.access_token ?? headers.access_token;
+
       const decodedAccessToken: TokenPayload =
-        await this.tokenService.getTokenPayload(cookies.access_token);
+        await this.tokenService.getTokenPayload(currentAccessToken);
       request.user = decodedAccessToken;
       return true;
     } catch (accessTokenError) {
       try {
+        const currentRefreshToken = cookies.refresh_token ?? headers.refresh_token;
+
         const decodedRefreshToken: TokenPayload =
-          await this.tokenService.getTokenPayload(cookies.refresh_token);
+          await this.tokenService.getTokenPayload(currentRefreshToken);
         const newAccessToken = await this.tokenService.signAsync(
           decodedRefreshToken,
           process.env.ACCESS_TOKEN_EXPIRE,
         );
         request.user = decodedRefreshToken;
         response.cookie("access_token", newAccessToken, { ...cookieOptions});
+        response.header("access_token", newAccessToken);
         return true;
       } catch (refreshTokenError) {
         if (isPublic) {
